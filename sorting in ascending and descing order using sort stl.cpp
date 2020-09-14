8e28c3c25f06c5b79324ef3296b1ef4d1c320470
Arnavjain1703 <arnavjainkht@gmail.com>
2020-09-15 01:19:35 +0530

    Sorting array using Stl

---
+++ sorting in ascending and descing order using sort stl.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+  #ifndef ONLINE_JUDGE
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+   #endif
+   int a[5]={5,3,4,2,1};
+                                        // sorting in ascending order
+   sort(a,a+5);                         
+   for(int i=0;i<5;i++)
+   {
+     cout<<a[i]<<'\n';
+   }
+   
+   sort(a,a+5,greater<int>());          // sorting in decending order
+   for(int i=0;i<5;i++)
+   {
+     cout<<a[i]<<'\n';
+   }
+
+}
