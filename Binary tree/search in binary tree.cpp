7b8b8e6371569eb48b7e02fb82b18825fd5e9687
Arnavjain1703 <arnavjainkht@gmail.com>
2020-10-16 19:50:19 +0530

    Search in binary tree

---
+++ Binary tree/search in binary tree.cpp
@@ -0,0 +1,19 @@
+bool search(Node* root, int x)
+{
+    if(root==NULL)
+    {
+        return  false;
+    }
+    if(root->data==x)
+    {
+        return true;
+    }
+    if(root->data>x)
+    {
+       return  search(root->left,x);
+    }
+    if(root->data<x)
+    {
+       return  search(root->right,x);
+    }
+}
