dc8eb577519497af0c9cdad3084bf9f58442c1ed
Arnavjain1703 <arnavjainkht@gmail.com>
2020-10-16 19:44:07 +0530

    INsert in binary tree

---
+++ Binary tree/Insert in binary tree.cpp
@@ -0,0 +1,17 @@
+Node* insert(Node* root, int Key)
+{
+     if(root==NULL)
+     {
+         return new Node(Key);
+     }
+     if(root->data>Key)
+     {
+       root->left=insert(root->left, Key);
+     }
+    else if(root->data<Key)
+     {
+        root->right=insert(root->right, Key);
+     }
+     return root;
+     
+}
