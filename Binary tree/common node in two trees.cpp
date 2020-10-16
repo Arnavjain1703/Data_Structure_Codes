dd0a1f725d44264951c7ac65fb0ea5674ebf368f
Arnavjain1703 <arnavjainkht@gmail.com>
2020-10-16 20:16:02 +0530

    common nodes in two trees

---
+++ Binary tree/common node in two trees.cpp
@@ -0,0 +1,38 @@
+void countDuplicate(Node *root, unordered_set<int> &s, vector<int> &duplicate)
+{
+    if(root)
+    {
+        countDuplicate(root->left, s, duplicate);
+        
+        if(s.find(root->data) != s.end())
+        {
+            duplicate.push_back(root->data);
+        }
+        
+        countDuplicate(root->right, s, duplicate);
+    }
+}
+
+void createSet(Node *root, unordered_set<int> &s)
+{
+    if(root)
+    {
+        createSet(root->left, s);
+        
+        s.insert(root->data);
+        
+        createSet(root->right, s);
+    }
+}
+
+vector<int> printCommon(Node *root1, Node *root2)
+{
+    unordered_set<int> s;
+    createSet(root1, s);
+    
+    vector<int> duplicate;
+    countDuplicate(root2, s, duplicate);
+    
+    return duplicate ;
+}
+
