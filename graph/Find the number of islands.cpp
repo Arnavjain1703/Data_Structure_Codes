018724954f45a4f881d630d74e5f8e25dc06b5a6
Arnavjain1703 <arnavjainkht@gmail.com>
2020-11-20 13:41:32 +0530

    Find no of islands

---
+++ graph/Find the number of islands.cpp
@@ -0,0 +1,31 @@
+void dfs(vector<int>A[],int n,int m,vector<vector<bool>>&vis,int i,int j)
+{
+    vis[i][j]=true;
+    for(int x=i-1;x<=i+1;x++)
+    {
+        for(int y=j-1;y<=j+1;y++)
+        {
+            if(x>=0 && y>=0 && x<n && y<m)
+            {
+                if(!(x==i && y==j)&&A[x][y]==1 && !vis[x][y])
+                dfs(A,n,m,vis,x,y);
+            }
+        }
+    }
+}
+int findIslands(vector<int> A[], int N, int M) {
+    vector<vector<bool>>vis(N,vector<bool>(M,false));
+    int count=0;
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            if(A[i][j]==1 && !vis[i][j])
+            {
+                count++;
+                dfs(A,N,M,vis,i,j);
+            }
+        }
+    }
+    return count;
+}
