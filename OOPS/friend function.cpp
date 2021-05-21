95850578c952935aab379b581a7d5837c85eb41e
Arnavjain1703 <arnavjainkht@gmail.com>
2021-05-21 14:02:07 +0530

    Friend Function

---
+++ OOPS/friend function.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+class Human{
+  private:
+  string iname;
+  int iage;
+  public:
+  Human(string name,int age)
+  {
+    iname=name;
+    iage=age;
+  }
+  friend void display(Human man);
+};
+ void display(Human man)
+{
+  cout<<man.iname<<" "<<man.iage<<'\n';
+}
+int main() {
+  #ifndef ONLINE_JUDGE
+    freopen("inputf.in","r",stdin);
+    freopen("outputf.in","w",stdout);
+  #endif  
+  Human name("Arnav",22);
+  display(name);  
+}
+
+
+// A friend function of a class is defined outside that class' scope but it has the right to access all private and protected members of the class. Even though the prototypes for friend functions appear in the class definition, friends are not member functions.
